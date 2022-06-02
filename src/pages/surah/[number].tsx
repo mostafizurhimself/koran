c445ee175900b841ffffb10cc00e708dfb92d122
MdMostaFizurRahaman <asgraphicsolution@gmail.com>
2022-06-02 16:36:01 +0600

    Add html audio player

--- src/pages/surah/[number].tsx
+++ src/pages/surah/[number].tsx
@@ -2,49 +2,36 @@ import AppLayout from '@/layouts/AppLayout';
 import { SurahAudio } from '@/types';
 import axios from 'axios';
 import { GetServerSideProps, NextPage } from 'next';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import { useRef, useState } from 'react';
 
 interface Props {
   surah: SurahAudio;
 }
 
 const Surah: NextPage<Props> = ({ surah }) => {
-  const [currentAyah, setCurrentAyah] = useState(1);
+  const audioElement = useRef<HTMLAudioElement>(null);
 
-  const getCurrentAudioUrl = () => {
+  const getCurrentAudioUrl = (currentAyah = 1) => {
     return surah.ayahs[currentAyah - 1].audio;
   };
 
-  useEffect(() => {
-    console.log(getCurrentAudioUrl());
-  }, []);
+  const playNextAudio = () => {
+    const currentAyah = audioElement?.current?.getAttribute('data-ayah') || '1';
+    const nextAyah = parseInt(currentAyah) + 1;
+  };
 
   return (
     <AppLayout title="Surah">
-      {/* <audio controls loop={true} id="audio" preload="none" tabIndex={0}>
-        <source src="/audio/001/001000.mp3" />
-        <source src="/audio/001/001001.mp3" />
-        <source src="/audio/001/001002.mp3" />
+      <audio ref={audioElement} src={getCurrentAudioUrl()} data-ayah="1" autoPlay controls onEnded={playNextAudio}>
         Your browser does not support HTML5 audio.
-      </audio> */}
-      <AudioPlayer
-        autoPlay
-        preload="auto"
-        src={getCurrentAudioUrl()}
-        crossOrigin="http://api.alquran.cloud/"
-        onPlay={(e) => console.log('onPlay')}
-        // other props here
-      />
+      </audio>
     </AppLayout>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const currentSurah = context.params?.number;
-  const res = await axios.get(`http://api.alquran.cloud/v1/surah/${currentSurah}/ar.alafasy`);
+  const res = await axios.get(`http://api.alquran.cloud/v1/surah/${currentSurah}/ar.abdurrahmaansudais`);
   const data = (await res.data.data) as SurahAudio;
   return {
     props: {
