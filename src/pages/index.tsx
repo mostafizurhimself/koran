90ced81f6a5dfa9480a77057d44f968037824eb6
Md Mostafizur Rahman <mostafizurhimself@gmail.com>
2022-06-06 19:45:37 +0600

    Add about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import AboutSection from '@/components/partials/AboutSection';
 import HeroSection from '@/components/partials/HeroSection';
 import DefaultLayout from '@/layouts/DefaultLayout';
 import type { NextPage } from 'next';
@@ -6,6 +7,7 @@ const Home: NextPage = () => {
   return (
     <DefaultLayout title="Koran.co">
       <HeroSection />
+      <AboutSection />
     </DefaultLayout>
   );
 };
